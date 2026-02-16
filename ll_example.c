b86c00be05e4d7cd167270f83d9f3570476ab0a8
Tanawat Wongwad <63237781+KhaiTomGut2006@users.noreply.github.com>
2026-02-16 15:30:20 +0700

    .

--- ll_example.c
+++ ll_example.c
@@ -1,57 +1,50 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "ll.h"
 
 int main( void )
-{ 
-   LLPtr startPtr = NULL; 
-   unsigned int choice; 
-   int id; 
-   char name[50]; 
+{
+   LLPtr startPtr = NULL;
+   unsigned int choice;
+   int id;
+   char name[50];
 
-   instructions(); 
-   printf( "%s", "? " );
-   scanf( "%u", &choice );
-
-   while ( choice != 3 ) { 
-
-      switch ( choice ) { 
+   instructions();
+   printf("? ");
+   scanf("%u", &choice);
+   while (choice != 3) {
+      switch (choice) {
          case 1:
-            printf( "%s", "Enter id and name: " );
-            scanf( "%d %s", &id, name );
-            insert( &startPtr, id, name );
-            printList( startPtr );
+            printf("Enter id and name: ");
+            scanf("%d", &id);
+            getchar(); // consume whitespace/newline
+            fgets(name, sizeof(name), stdin);
+            name[strcspn(name, "\n")] = 0;
+            insert(&startPtr, id, name);
+            printList(startPtr);
             break;
-         case 2: 
-            if ( !isEmpty( startPtr ) ) { 
-               printf( "%s", "Enter number to be deleted: " );
-               scanf( "%d", &id );
-
-               
-               if ( deletes( &startPtr, id ) ) { 
-                  printf( "%d deleted.\n", id );
-                  printList( startPtr );
-               } 
-               else {
-                  printf( "%d not found.\n\n", id );
-               } 
-            } 
-            else {
-               puts( "List is empty.\n" );
-            } 
-
+         case 2:
+            if (!isEmpty(startPtr)) {
+               printf("Enter number to be deleted: ");
+               scanf("%d", &id);
+               if (deletes(&startPtr, id)) {
+                  printf("%d deleted.\n", id);
+                  printList(startPtr);
+               } else {
+                  printf("%d not found.\n\n", id);
+               }
+            } else {
+               puts("List is empty.\n");
+            }
             break;
          default:
-            puts( "Invalid choice.\n" );
+            puts("Invalid choice.\n");
             instructions();
             break;
-      } 
-
-      printf( "%s", "? " );
-      scanf( "%u", &choice );
+      }
+      printf("? ");
+      scanf("%u", &choice);
    }
-   
-   clearList( &startPtr );
-   puts( "End of run." );
-} 
+   clearList(&startPtr);
+   puts("End of run.");
+}
